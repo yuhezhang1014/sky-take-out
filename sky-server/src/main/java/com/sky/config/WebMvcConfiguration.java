7b3e98c9a5df6230891e231175243bdb79fb4b88
yuhezhang1014 <yuhezhang74@gmail.com>
2026-03-02 02:31:00 +0800

    员工分页查询功能改善 第一次上传的漏了一个逗号

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -78,7 +78,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      * @param converters
      */
     protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        log.info("扩展消息转换器")
+        log.info("扩展消息转换器");
         // 创建一个消息转换器对象
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         // 需要为消息转换器设置一个对象转换器，对象转换器可以将Json对象序列化为json数据
