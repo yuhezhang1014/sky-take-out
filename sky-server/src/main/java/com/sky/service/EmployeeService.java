5f3a4d5aab882de2d2291f1a4783d5f052afe789
yuhezhang1014 <yuhezhang74@gmail.com>
2026-03-03 04:39:29 +0800

    编辑员工业务代码的开发

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -35,4 +35,19 @@ public interface EmployeeService {
      * @param id
      */
     void startOrStop(Integer status, Long id);
+
+
+    /**
+     * 根据id查员工
+     * @param id
+     * @return
+     */
+    Employee getById(Long id);
+
+
+    /**
+     * 编辑员工信息
+     * @param employeeDTO
+     */
+    void update(EmployeeDTO employeeDTO);
 }
